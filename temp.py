1910d382396d6b44e0e8be350a14d33d1eaa92b9
Arya Khanolkar <115803659+AKhanolkar14@users.noreply.github.com>
2024-01-23 17:50:00 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,13 @@
+
+from tkinter import*
+root= Tk()
+root.title("Addition")
+root.geometry("400x200")
+root.mainloop()
+
+
+from tkinter import *
+root= Tk()
+root.title("Fibonacci")
+root.geometry("400x400")
+root.mainloop()
